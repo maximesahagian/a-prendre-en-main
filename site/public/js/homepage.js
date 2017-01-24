d9c413ba274ffa34420262993cdfecdfbcc2b21e
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-01-24 18:53:13 +0100

    Add pourcentage of goal

--- site/public/js/homepage.js
+++ site/public/js/homepage.js
@@ -1,5 +1,12 @@
 
 $.get('https://api.ulule.com/v1/projects/'+$('.ulule_value').attr('ulule'), function(data){
-   $('.amount-raised').append(data.amount_raised);
-   $('.goal').append(data.goal);
+   $('.amount-raised').html(data.amount_raised);
+   $('.goal').html(data.goal);
+
+   var pourcentage = (data.amount_raised/data.goal)*100;
+   pourcentage = Math.round(pourcentage);
+
+   $('.goal-bar').css({
+      width: pourcentage
+   })
 });
