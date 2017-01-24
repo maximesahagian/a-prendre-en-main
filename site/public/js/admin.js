8be9461cd2754e066b0a3a79e8040b7b18774ae8
Rob <utilisateur@BR.local>
2017-01-24 11:38:57 +0100

    burger animation correction

--- site/public/js/admin.js
+++ site/public/js/admin.js
@@ -2,19 +2,17 @@ var opensidebar = false;
 $( document ).ready(function() {
     $('#arrow-open').click(function() {
         opensidebar = true;
-        $('.sidebar').animate({
+        $('.sidebar').stop().animate({
 
-            left:'0',
-            zIndex:'1'
-        }, 1000
+            left:'0'
+        }, 'fast'
         );
     });
     $('#arrow-close').click(function() {
         opensidebar = false;
-        $('.sidebar').animate({
-                left:'-260px',
-                zIndex:'1'
-            }, 1000
+        $('.sidebar').stop().animate({
+                left:'-260px'
+            }, 'fast'
         );
     });
 });
@@ -23,17 +21,15 @@ var onresize = function(e) {
     //note i need to pass the event as an argument to the function
     width = e.target.outerWidth;
     if (width >= 1024 && !opensidebar) {
-        $('.sidebar').animate({
-                left:'0',
-                zIndex:'1'
-            }, 1000
+        $('.sidebar').stop().animate({
+                left:'0'
+            }, 'fast'
         );
     }
     else if (width < 1024) {
-        $('.sidebar').animate({
-                left:'-260px',
-                zIndex:'1'
-            }, 1000
+        $('.sidebar').stop().animate({
+                left:'-260px'
+            }, 'fast'
         );
     }
 
