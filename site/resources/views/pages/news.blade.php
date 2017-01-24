098c9238b64129c7f996a99c7107b17028270620
Nabeth Kévin <nabethkevin@MacBook-Pro-de-Nabeth.local>
2017-01-24 15:53:09 +0100

    BO- News style

--- site/resources/views/pages/news.blade.php
+++ site/resources/views/pages/news.blade.php
@@ -9,24 +9,30 @@
                 </div>
                 <div class="panel-body">
                     <a href="/admin/news/add"><button class="btn btn-success add-new-btn">Ajouter</button></a>
-                    @foreach($news as $new)
                     <div class="row">
-                        <div class="col-md-12">
+                    @foreach($news as $new)
+                        <div class="col-sm-4">
                             <div class="metric">
                                     <a href="/admin/news/edit/{{$new->id}}">
                                         {{$new->title}}
                                     </a>
-                                <div class="actions">
-                                    <a href="/admin/news/edit/{{$new->id}}"><button class="btn btn-alert">Editer</button></a>
-                                    <button to-delete="{{$new->id}}" class="btn btn-danger delete-new">Supprimer</button>
-                                </div>
+
                                 <div class="image-new" style="background-image: url('/img/actu/<?=$new->image?>')">
 
                                 </div>
+                                <div class=" row">
+                                    <div class="col-xs-6">
+                                     <a href="/admin/news/edit/{{$new->id}}"><button class="btn btn-alert">Editer</button></a>
+                                    </div>
+                                    <div class="col-xs-6">
+                                      <button to-delete="{{$new->id}}" class="btn btn-danger delete-new">Supprimer</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+
                     @endforeach
+                    </div>
                 </div>
             </div>
         </div>
