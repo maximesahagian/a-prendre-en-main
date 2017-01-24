7deff77874422ce154fba75841ed859ea3bf94cd
Nabeth Kévin <nabethkevin@MacBook-Pro-de-Nabeth.local>
2017-01-24 16:41:22 +0100

    home video

--- site/resources/views/pages/home.blade.php
+++ site/resources/views/pages/home.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.page')
 @section('content')
+
     <div class="home-page">
+        <div class="video-conainer">
+            <video loop autoplay>
+                <source src="/film/filmcompress.mp4" type="video/mp4">
+            </video>
+        </div>
         <p class="para">Page d'accueil</p>
+
     </div>
 @stop
