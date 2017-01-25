c018eddee78e3f6ce3093119875d5a8660ceb777
Maxime Sahagian <msahagian@big-boss-studio.com>
2017-01-25 10:19:27 +0100

    Add debug scroll magic

--- site/resources/views/layouts/page.blade.php
+++ site/resources/views/layouts/page.blade.php
@@ -11,6 +11,7 @@
     @yield('content')
 
 
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
     <script src="/js/jquery.js"></script>
