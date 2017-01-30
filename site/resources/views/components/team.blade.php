a8c4be53e7b529797749136da940f4be6d4d9400
kevin nabeth <kgnabeth@gmail.com>
2017-01-30 19:48:42 +0100

    equipe hover

--- site/resources/views/components/team.blade.php
+++ site/resources/views/components/team.blade.php
@@ -5,6 +5,19 @@
         <?php $i = 1; ?>
         @foreach($team as $member)
             <div class="image-profile" style="background: url('img/members/{{$member->image}}'); background-size: cover; background-position: center;">
+                <div class="team-description">
+                    <h3>
+                        Estefania
+                    </h3>
+                    <h4>
+                        Secrétaire Adjointe de l’association
+
+                    </h4>
+                    <p>
+                        Cette espagnole travaille comme Kinésithérapeute à Paris depuis plusieurs années. Sa principale motivation, c’est de donner les moyens aux autres de se remettre debout et d’avancer seul.
+                    </p>
+
+                    </div>
                 <div class="color color-{{$i}}">
 
                 </div>
