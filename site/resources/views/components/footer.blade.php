6bda92a1526ee40a14916e4dbfa62f9b6933fc52
Robin BELVEDERE <rob@MacBook-Pro-de-Robin.local>
2017-03-07 10:00:22 +0100

    Retours footer

--- site/resources/views/components/footer.blade.php
+++ site/resources/views/components/footer.blade.php
@@ -1,7 +1,11 @@
 <footer class="footer-section">
     <ul class="partenaires">
-        <li class="digital_village"><img src="/img/digital_village_footer.png" alt=""></li>
-        <li class="supdeco"><img src="/img/supdeco_footer.png" alt=""></li>
-        <li class="attineos"><img src="/img/attineos_footer.png" alt=""></li>
+        <li class="saintjacques"><a href="http://www.saintjacquesduhautpas.com/">Saint-Jacques du Haut-Pas</a></li>
+        <li class="accessrepro"><a href="http://accessrepro.fr/"><img src="/img/accessrepro.png" alt=""></a></li>
+        <li class="coworkshop"><a href="http://www.coworkshop.fr/"><img src="/img/coworkshop.png" alt=""></a></li>
+    </ul>
+    <ul class="liens">
+        <li class="facebook"><a href="https://www.facebook.com/Aprendreenmain/"><img src="/img/facebook.png" alt=""></a></li>
+        <li class="twitter"><a href="https://twitter.com/A_PEM"><img src="/img/twitter.png" alt=""></a></li>
     </ul>
 </footer>
